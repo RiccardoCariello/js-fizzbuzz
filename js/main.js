98db6dfdb4cfb193b95152bf8ab6b6b9826b98a6
MillionDollarBob <riccardo.c1995@outlook.com>
2023-03-30 16:29:31 +0200

    Aggiustamenti e correzioni

--- js/main.js
+++ js/main.js
@@ -14,29 +14,50 @@ const buzz = "Buzz";
 //ora credo due variabili che avranno il compito di immagazzinare il resto della divisione tra "i" e i numeri "3" e "5"
 let multiplier3;
 let multiplier5;
-//la variabile result servirà ad immagazzianare "fizz" e/o "buzz" nel caso in cui fossero divisori
-let result='';
-//iniziamo il cilo
+
+//iniziamo il ciclo
 for(let i=1; i<=100; i++){
-    //calcolo se "i" è multipla di 3 e/o 5
+    //effettuiamo l'operazione per capire se al momento i è un multiplo
     multiplier3= i%3;
     multiplier5= i%5;
-    //con questo primo if vediamo subito se "i" è multipla di 3 e nel caso ce lo "appuntiamo" nella variabile result
-    if(multiplier3==0){
-    result+=fizz;
-    } //ora vediamo se è divisibile per 5
-    if(multiplier5==0){
-    result+=buzz;
-    } 
-    //qui controlliamo il da farsi, se una delle due variabili "multiplier" è = a zero allora sappiamo che i è divisibile per almeno uno dei due valori,
-    // perciò stampiamo result, per poi resettare il suo valore
-    if(multiplier3 == 0 || multiplier5 == 0){                                                  
-        console.log(result);
-        result='';        
-    }else{ //se così non fosse stampiamo semplicemente i
+    
+    if(multiplier3 == 0 && multiplier5 == 0) //con questo primo if vediamo subito se i è multiplo di entrambi
+    {
+        console.log(fizz + buzz);
+        
+        document.getElementById("my-container").innerHTML += `
+        <div class="squareFizzBuzz">
+            <p>${fizz + buzz}</p>
+        </div>
+        `;
+    }else if(multiplier3==0){   //qui vediamo se è multiplo di 3
+        
+        console.log(fizz);
+        document.getElementById("my-container").innerHTML += `
+        <div class="squareFizz">
+            <p>${fizz}</p>
+        </div>
+        `;
+    }else if(multiplier5==0){   //qui vediamo se lo è di 5
+        
+        console.log(buzz);
+        document.getElementById("my-container").innerHTML += `
+        <div class="squareBuzz">
+            <p>${buzz}</p>
+        </div>
+        `;
+    }else{ //se arriviamo a questo else allora significa che i non è un multiplo 
         console.log(i);
+        document.getElementById("my-container").innerHTML += `
+        <div class="squareNormal">
+            <p>${i}</p>
+        </div>
+        `;
     }
+    
+    
 
     
 
 }
+
